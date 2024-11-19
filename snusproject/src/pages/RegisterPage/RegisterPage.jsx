b0246edf15693b65c755b0dbac3f0cd27f49636c
J0113boy <keskitalo_6@hotmail.com>
2024-11-19 17:58:54 +0100

    Create handleChange and handleSubmit functions for RegisterPage

--- snusproject/src/pages/RegisterPage/RegisterPage.jsx
+++ snusproject/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,29 +1,89 @@
 import "./RegisterPage.css"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    localStorage.setItem("userData", JSON.stringify(formData))
+    alert("Användare registrerad! Data sparad lokalt")
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    })
+  }
+
   return (
     <div className="register-container">
       <h1 className="register-title">Registrera dig</h1>
       <form className="register-form">
         <div className="form-group">
           <label htmlFor="firstName">Förnamn</label>
-          <input type="text" id="firstName" name="firstName" required />
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="lastName">Efternamn</label>
-          <input type="text" id="lastName" name="lastName" required />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" required />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Lösenord</label>
-          <input type="text" id="password" name="password" required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="lower-buttons">
-          <button type="submit" className="register-button">
+          <button
+            type="submit"
+            className="register-button"
+            onClick={handleSubmit}
+          >
             Registrera
           </button>
           <Link to="/">
