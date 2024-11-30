7e0dfc8460f8d2b834b8ac3e21a310c940768272
J0113boy <keskitalo_6@hotmail.com>
2024-11-30 15:46:19 +0100

    Add simple userModel

--- snusproject/Backend/controllers/userController.js
+++ snusproject/Backend/controllers/userController.js
@@ -1,10 +1,11 @@
-const User = require('../models/userModel') // behöver skapas 
+const User = require('../models/userModel') 
 
 exports.getAllUsers = async (req, res) => {
     try {
         const users = await User.find()
-        if (!users) {
-            res.status(404).json({
+
+        if (users.length === 0) {
+            res.status(200).json({
                 message: 'No users found in the database'
             })
         }
